56bd273a0c5863ff3a175d8034e5073204b6bf7f
Daan van Ham <daan@vanham.io>
2016-10-26 12:19:11 +0200

    ES6 🤓

--- test/config.js
+++ test/config.js
@@ -1,25 +1,25 @@
 'use strict';
 
-var Lab = require('lab'),
+const Lab = require('lab'),
 	Code = require('code'),
 	lab = exports.lab = Lab.script(),
 	config = require('lib/index');
 
-lab.experiment('Config', function() {
-	lab.experiment(':get', function() {
-		lab.test('should find the key if it exists', function(done) {
+lab.experiment('Config', () => {
+	lab.experiment(':get', () => {
+		lab.test('should find the key if it exists', (done) => {
 			Code.expect(config.get('name')).to.equal('Peafowl');
 
 			done();
 		});
 
-		lab.test('should find nested config', function(done) {
+		lab.test('should find nested config', (done) => {
 			Code.expect(config.get('path/to/nested')).to.equal('thingy');
 
 			done();
 		});
 
-		lab.test('should throw Error when it can\'t find the config', function(done) {
+		lab.test('should throw Error when it can\'t find the config', (done) => {
 			try {
 				config.get('does/not/exist');
 			}
@@ -31,22 +31,22 @@ lab.experiment('Config', function() {
 		});
 	});
 
-	lab.experiment(':set', function() {
-		lab.test('should set config', function(done) {
+	lab.experiment(':set', () => {
+		lab.test('should set config', (done) => {
 			config.set('test', 'example');
 			Code.expect(config.get('test')).to.equal('example');
 
 			done();
 		});
 
-		lab.test('should set nested config', function(done) {
+		lab.test('should set nested config', (done) => {
 			config.set('path/to/config', 'example');
 			Code.expect(config.get('path/to/config')).to.equal('example');
 
 			done();
 		});
 
-		lab.test('should be able to add to existing config', function(done) {
+		lab.test('should be able to add to existing config', (done) => {
 			config.set('manifest/test', 'example');
 			Code.expect(config.get('manifest/test')).to.equal('example');
 
